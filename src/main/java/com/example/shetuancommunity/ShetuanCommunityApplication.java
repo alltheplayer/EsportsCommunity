e4509a9798910a9393f3d495f94d2f33b8d02835
alltheplayer <1398305759@qq.com>
2022-04-23 15:44:40 +0800

    init report

---
+++ src/main/java/com/example/shetuancommunity/ShetuanCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.shetuancommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShetuanCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShetuanCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shetuancommunity/ShetuanCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shetuancommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShetuanCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
